6dc107d71c0e2fc0f2bc03b11234667044cb69d6
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2025-10-02 23:17:23 -0500

    Added import statements

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from fastapi import FastAPI, UploadFile, File, HTTPException, Header
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List, Optional
+from PIL import Image, UnidentifiedImageError
+import io
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import torch
+import torch.nn.functional as F
+from torchvision.models import resnet18, ResNet18_Weights
